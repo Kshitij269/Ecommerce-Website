c885bba4e29fa493f8cdeb0fd0e2b823e7b27445
sks269 <kshitij@shuniyavigyan.com>
2024-08-19 00:01:59 +0530

    Store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,8 @@ import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import Contact from "./components/layout/Contact/Contact.jsx";
 import About from "./components/layout/About/About.jsx";
-
+import store from "./store";
+import { Provider } from "react-redux";
 
 const router = createBrowserRouter([
   {
@@ -22,16 +23,14 @@ const router = createBrowserRouter([
       },
       {
         path: "about",
-        element: <About/>
+        element: <About />,
       },
-      
-      
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </Provider>
 );
