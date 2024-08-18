7d20afde87b6f19fca87f517f3f4218258002351
Shiv-2018 <shivindra2018@gmail.com>
2024-08-19 00:59:57 +0530

    Pull

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,32 +1,19 @@
 import React from "react";
+import Product from "./Product";
 import "./Home.css";
-import Product from "./ProductCard";
+import { CgMouse } from "react-icons/cg";
 
-<<<<<<< HEAD
-import "./Home.css";
-// import { CgMouse } from "react-icons/cg";
-
-// const product = {
-//   name: T - shirt,
-//   price: 20.99,
-//   image: [{
-//       url: "https://png.pngtree.com/png-vector/20220527/ourmid/pngtree-unisex-bright-blue-t-shirt-mock-up-png-image_4757174.png",
-//     },
-//   ],
-//   _id: "abhishek",
-// };
-
-=======
 const product = {
-  name: "T-shirt",
+  name: T - shirt,
   price: 20.99,
-  images: [{ url: "https://png.pngtree.com/png-vector/20220527/ourmid/pngtree-unisex-bright-blue-t-shirt-mock-up-png-image_4757174.png" }],
+  image: [
+    {
+      url: "https://png.pngtree.com/png-vector/20220527/ourmid/pngtree-unisex-bright-blue-t-shirt-mock-up-png-image_4757174.png",
+    },
+  ],
   _id: "abhishek",
-  ratings: 4.5,  // Add these properties if you want to use them
-  numOfReviews: 10,
 };
 
->>>>>>> c885bba4e29fa493f8cdeb0fd0e2b823e7b27445
 const Home = () => {
   return (
     <>
@@ -34,9 +21,9 @@ const Home = () => {
         <p>Welcome to Ecommerce</p>
         <h1>Find amazing products below</h1>
         <a href="#container">
-          {/* <button>
+          <button>
             Scroll <CgMouse />
-          </button> */}
+          </button>
         </a>
       </div>
       <h2 className="homeHeading">Featured Products</h2>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-
+import "./store.js"
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import Contact from "./components/layout/Contact/Contact.jsx";
 import About from "./components/layout/About/About.jsx";
-<<<<<<< HEAD
-=======
-import store from "./store";
+
 import { Provider } from "react-redux";
->>>>>>> c885bba4e29fa493f8cdeb0fd0e2b823e7b27445
+
 
 const router = createBrowserRouter([
   {
