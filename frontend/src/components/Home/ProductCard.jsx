b8ddb178a5065e592d685b7dd4f8a8b38a30fbd3
sks_269 <skshitij269@gmail.com>
2024-08-21 04:34:36 +0530

    Images Fixed

--- frontend/src/components/Home/ProductCard.jsx
+++ frontend/src/components/Home/ProductCard.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Rating from "react-rating";
-
-const options = {
-  edit: false,
-  color: "rgba(20,20,20,0.1)",
-  activeColor: "tomato",
-  value: 2.5,
-  isHalf: true,
-  size: window.innerWidth < 600 ? 20 : 25,
-};
+import { Rating } from "@material-ui/lab";
 
 const ProductCard = ({ product }) => {
+  const options = {
+    value: product.ratings,
+    readOnly: true,
+    precision: 0.5,
+  };
   return (
     <Link className="productCard" to={`/product/${product._id}`}>
       <img src={product.images[0].url} alt={product.name} />
